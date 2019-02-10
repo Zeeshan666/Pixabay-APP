10790fd6ff77e498906011b74a44568219371972
Zeeshan Manzoor <zeeshanmanzoor.zm@gmal.com>
2019-02-11 03:07:04 +0500

    Updates

---
+++ precache-manifest.66634eb1350ee4b3b5fc70f2d1984fb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "215bf1c3ccdc83141728",
+    "url": "/Pixabay-APP/./static/js/runtime~main.215bf1c3.js"
+  },
+  {
+    "revision": "cff4040830456df4e93e",
+    "url": "/Pixabay-APP/./static/js/main.cff40408.chunk.js"
+  },
+  {
+    "revision": "f1a09ad5e51c84c7c9be",
+    "url": "/Pixabay-APP/./static/js/1.f1a09ad5.chunk.js"
+  },
+  {
+    "revision": "cff4040830456df4e93e",
+    "url": "/Pixabay-APP/./static/css/main.8d8325a5.chunk.css"
+  },
+  {
+    "revision": "d274c311d76fd17a98786aa8ea86a490",
+    "url": "/Pixabay-APP/./index.html"
+  }
+];
